25bb0394eeedfc779ef914d640d10e919ede8f6a
Golden Chimusinde <goldenchimsy@gmail.com>
2023-01-05 18:55:51 +0200

    Extracted Keyboard Helper methods and placed them in an extension

--- MemeMe 1.0/Controller/MemeEditorViewController.swift
+++ MemeMe 1.0/Controller/MemeEditorViewController.swift
@@ -45,36 +45,6 @@ class MemeEditorViewController: UIViewController, UINavigationControllerDelegate
         unsubscribeFromKeyboardNotifications()
     }
     
-    // MARK: Keyboard Notifications
-    func subscribeToKeyboardNotifications() {
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-    
-    func unsubscribeFromKeyboardNotifications() {
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-    
-    @objc func keyboardWillShow(_ notification: Notification) {
-        if txtFieldBottom.isEditing {
-            view.frame.origin.y = -getKeyboardHeight(notification)
-        }
-    }
-    
-    @objc func keyboardWillHide(_ notification: Notification) {
-        if view.frame.origin.y != 0 {
-            view.frame.origin.y = 0
-        }
-    }
-    
-    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
-        let userInfo = notification.userInfo
-        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
-        return keyboardSize.cgRectValue.height
-    }
-    
-    
     // MARK: TextFields
     func setUpTextField(textField: UITextField, text: String) {
         let memeTextAttributes = [
---
+++ MemeMe 1.0/Extensions/MemeEditorViewController+Ext.swift
@@ -0,0 +1,40 @@
+//
+//  MemeEditorViewController+Extension.swift
+//  MemeMe 1.0
+//
+//  Created by Golden Chimusinde on 2023/01/05.
+//
+
+import Foundation
+import UIKit
+
+extension MemeEditorViewController {
+    // MARK: Keyboard Notifications
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        if txtFieldBottom.isEditing {
+            view.frame.origin.y = -getKeyboardHeight(notification)
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        if view.frame.origin.y != 0 {
+            view.frame.origin.y = 0
+        }
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+}
