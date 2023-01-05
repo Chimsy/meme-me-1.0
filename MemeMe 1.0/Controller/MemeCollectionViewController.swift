57b912aeed1ef4d7819a5c1b93ab575e4dc6fbd0
Golden Chimusinde <goldenchimsy@gmail.com>
2023-01-05 18:05:46 +0200

    Added an Add Button

---
+++ MemeMe 1.0/Controller/MemeCollectionViewController.swift
@@ -0,0 +1,71 @@
+//
+//  MemeCollectionViewController.swift
+//  MemeMe 1.0
+//
+//  Created by Golden Chimusinde on 2023/01/05.
+//
+
+import UIKit
+
+
+class MemeCollectionViewController: UICollectionViewController {
+    
+    // MARK: Properties and Outlets
+
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    
+    var memes: [Meme]! {
+        let object = UIApplication.shared.delegate
+        let appDelegate = object as! AppDelegate
+        return appDelegate.memes
+    }
+    
+    // MARK: Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let space:CGFloat = 3.0
+        let width = 100.0
+        let height = 100.0
+        
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.minimumLineSpacing = space
+        flowLayout.itemSize = CGSize(width: width, height: height)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        collectionView.reloadData()
+    }
+    
+    // MARK: Collection View Data Source
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.memes.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MemeCollectionViewCell", for: indexPath) as! MemeCollectionViewCell
+            let meme = self.memes[(indexPath as NSIndexPath).row]
+        
+        cell.memeImageView?.image = meme.memedImage
+        return cell
+    }
+    
+    // MARK: Go to Meme Detail View
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let detailController = self.storyboard!.instantiateViewController(withIdentifier: "MemeDetailViewController") as! MemeDetailViewController
+        detailController.meme = self.memes[(indexPath as NSIndexPath).row]
+        self.navigationController!.pushViewController(detailController, animated: true)
+    }
+    
+    // MARK: Add button to Meme Editor
+    @IBAction func createMeme(_ sender: Any) {
+        performSegue(withIdentifier: "createFromCollection", sender: sender)
+    }
+    
+}
+
+
---
+++ MemeMe 1.0/View/MemeCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMe 1.0
+//
+//  Created by Golden Chimusinde on 2023/01/05.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var memeImageView: UIImageView!
+    
+}
